2218f1406f95d4f0ad238e90bc458dff6784cf19
sydneygoth <sgothenquist@hotmail.com>
2023-02-09 10:20:59 -0800

    new motors and main

---
+++ src/motor2.py
@@ -0,0 +1,42 @@
+#Motor 1 initialization set up
+state = 0
+
+def Motor2(reset):
+    while True
+        if state == 0
+            Motor2=MotorDriver(pyb.Pin.board.PC1,pyb.Pin.board.PA0,pyb.Pin.board.PA1,5)
+            encoder2=EncoderClass(pyb.Pin.board.PC6,pyb.Pin.board.PC7,8)
+            encoder2.zero()
+            time_step = 0.01
+            Theta_Set = 100000
+            KP = 0.01
+            count = 0
+            pwm2 = PWM_Calc()
+            pwm2.set_setpoint(Theta_Set)
+            pwm2.set_KP(KP)
+            state = 1
+            yield state
+            
+        elif state == 1
+            Theta_Act = encoder2.read()
+            PWM = pwm2.Run(Theta_Act)
+            Motor2.set_duty_cycle(PWM)
+            time.sleep(time_step) #updates 0.01s
+            count += 1
+            
+            if count = 400
+                state = 2
+            yield state
+            
+        elif state == 2
+            Motor2.set_duty_cycle(0)
+            pwm2.Print_Data()
+            state = 3
+            
+            yield state
+            
+        elif state == 3
+            if reset == True:
+                state = 0
+            yield state
+
